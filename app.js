a4aa3994d71919c602ec048dd79384d36cd495d3
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2021-06-23 18:21:22 +0530

    fix: read textarea input

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-var btnTranslate = document.querySelector('#btn-translate')
 
-btnTranslate.addEventListener("click",function clickHandler(){
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+
+console.log(txtInput)
+
+function clickHandler(){
     console.log('Clicked!');
-})
+    console.log("input " ,txtInput.value);
+};
+
+btnTranslate.addEventListener("click",clickHandler)
+
+
 
