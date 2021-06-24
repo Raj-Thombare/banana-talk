48ae0c35005883399d2d64c32d0dbac15eef1530
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2021-06-24 16:42:17 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-var btnTranslate = document.querySelector("#btn-translate")
-var txtInput = document.querySelector("#txt-input")
 
-console.log(txtInput)
 
 function clickHandler(){
-    console.log('Clicked!');
-    console.log("input " ,txtInput.value);
+    outputDiv.innerText = txtInput.value;
 };
 
-btnTranslate.addEventListener("click",clickHandler)
+btnTranslate.addEventListener("click", clickHandler);
+
+
+
+
 
 
 
