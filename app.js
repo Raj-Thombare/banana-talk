264ffb3059d703df404eb65c5efa375a311a7d3a
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2021-06-26 15:23:59 +0530

    update button text

--- app.js
+++ app.js
@@ -3,16 +3,8 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
-
 function clickHandler(){
     outputDiv.innerText = txtInput.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler);
-
-
-
-
-
-
-
